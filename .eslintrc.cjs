9d991f9fc0ef74cf43c3931c184c45bf797a55fa
Icaro Quadra <icaroquadra@gmail.com>
2024-04-23 12:23:54 -0300

    :package: add prettier and rules on eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,63 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', 'node_modules'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react-refresh', 'react', 'jsx-a11y', '@typescript-eslint'],
   rules: {
+    'no-console': 'warn',
+    "react/self-closing-comp": "error",
+    'react/no-unknown-property': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/alt-text': [
+      'warn',
+      {
+        elements: ['img'],
+        img: ['Image'],
+      },
+    ],
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'prettier/prettier': ["error", {
+      'printWidth': 80,
+      'tabWidth': 2,
+      'singleQuote': true,
+      'trailingComma': 'all',
+      'arrowParens': 'always',
+      'semi': false,
+      'endOfLine': 'auto',
+    }],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
   },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import { ThemeProvider } from "styled-components";
-import { useState } from "react";
-import { Button } from "./components/Button";
-import { defaultTheme } from "./styles/themes/default.ts";
-import { GlobalStyle } from "./styles/global.ts";
+import { ThemeProvider } from 'styled-components'
+import { useState } from 'react'
+import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default.ts'
+import { GlobalStyle } from './styles/global.ts'
 
 export function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
-      <ThemeProvider theme={defaultTheme}>
-          <Button variant="primary" onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-          </Button>
-          <Button
-              variant="secondary"
-              onClick={() => setCount((count) => count + 1)}
-          >
-              count is {count}
-          </Button>
-          <Button variant="danger" onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-          </Button>
-          <Button variant="success" onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-          </Button>
-          <GlobalStyle/>
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
-      </ThemeProvider>
-  );
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </Button>
+      <Button
+        variant="secondary"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        count is {count}
+      </Button>
+      <Button variant="danger" onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </Button>
+      <Button variant="success" onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </Button>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
