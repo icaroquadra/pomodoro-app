3942351cab68f6f3c7d73b7ce6578cf2fef3089c
Icaro Quadra <icaroquadra@gmail.com>
2024-04-24 07:53:36 -0300

    :sparkles: add DefaultLayout to routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import { History } from './pages/History.tsx'
 import { Home } from './pages/Home.tsx'
+import { DefaultLaoyout } from './layouts/DefaultLaoyout.tsx'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLaoyout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
